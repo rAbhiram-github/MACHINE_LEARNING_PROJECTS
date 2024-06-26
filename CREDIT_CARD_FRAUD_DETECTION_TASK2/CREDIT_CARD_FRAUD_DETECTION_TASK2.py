71d758e280a44288dad1236257fe1ea384a16001
rAbhiram-github <24f1002043@ds.study.iitm.ac.in>
2024-06-26 10:45:10 +0530

    Add files via upload

---
+++ CREDIT_CARD_FRAUD_DETECTION_TASK2/CREDIT_CARD_FRAUD_DETECTION_TASK2.py
@@ -0,0 +1,97 @@
+
+# AUTHOR: ABHIRAM R
+# TASK NAME: CREDIT CARD FRAUD DETECTION
+# TASK CATEGORY: MACHINE LEARNING
+# GITHUB REPOSITORY: https://github.com/rAbhiram-github/CODSOFT
+
+# Importing libraries
+import pandas as pd 
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+
+# Loading data
+train = pd.read_csv("fraudTrain.csv")
+test = pd.read_csv("fraudTest.csv")
+data = pd.concat([train, test])
+
+# Encoding categorical variables
+from sklearn.preprocessing import LabelEncoder
+label_encoders = {}
+
+label_encode_cols = ['merchant', 'category', 'gender', 'state', 'job']
+for col in label_encode_cols:
+    le = LabelEncoder()
+    data[col] = le.fit_transform(data[col])
+    label_encoders[col] = le
+    
+    train[col] = le.fit_transform(train[col])
+    label_encoders[col] = le
+    
+    test[col] = le.fit_transform(test[col])
+    label_encoders[col] = le
+
+# Feature engineering
+data['trans_date_trans_time'] = pd.to_datetime(data['trans_date_trans_time'])
+data['dob'] = pd.to_datetime(data['dob'])
+
+data['transaction_year'] = data['trans_date_trans_time'].dt.year
+data['transaction_month'] = data['trans_date_trans_time'].dt.month
+data['transaction_day'] = data['trans_date_trans_time'].dt.day
+data['transaction_hour'] = data['trans_date_trans_time'].dt.hour
+
+data['birth_year'] = data['dob'].dt.year
+data['birth_month'] = data['dob'].dt.month
+data['birth_day'] = data['dob'].dt.day
+data.drop(['trans_date_trans_time', 'dob'], axis=1, inplace=True)
+
+
+train['trans_date_trans_time'] = pd.to_datetime(train['trans_date_trans_time'])
+train['dob'] = pd.to_datetime(train['dob'])
+
+train['transaction_year'] = train['trans_date_trans_time'].dt.year
+train['transaction_month'] = train['trans_date_trans_time'].dt.month
+train['transaction_day'] = train['trans_date_trans_time'].dt.day
+train['transaction_hour'] = train['trans_date_trans_time'].dt.hour
+
+train['birth_year'] = train['dob'].dt.year
+train['birth_month'] = train['dob'].dt.month
+train['birth_day'] = train['dob'].dt.day
+train.drop(['trans_date_trans_time', 'dob'], axis=1, inplace=True)
+
+test['trans_date_trans_time'] = pd.to_datetime(test['trans_date_trans_time'])
+test['dob'] = pd.to_datetime(test['dob'])
+
+test['transaction_year'] = test['trans_date_trans_time'].dt.year
+test['transaction_month'] = test['trans_date_trans_time'].dt.month
+test['transaction_day'] = test['trans_date_trans_time'].dt.day
+test['transaction_hour'] = test['trans_date_trans_time'].dt.hour
+test['birth_year'] = test['dob'].dt.year
+test['birth_month'] = test['dob'].dt.month
+test['birth_day'] = test['dob'].dt.day
+test.drop(['trans_date_trans_time', 'dob'], axis=1, inplace=True)
+
+# Dropping irrelevant columns
+data.drop(['first', 'last', 'street', 'city', 'trans_num'], axis=1, inplace=True)
+train.drop(['first', 'last', 'street', 'city', 'trans_num'], axis=1, inplace=True)
+test.drop(['first', 'last', 'street', 'city', 'trans_num'], axis=1, inplace=True)
+
+# Preparing data for modelling
+X = data.drop('is_fraud', axis=1)
+y = data['is_fraud']
+
+# Splitting data
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Training logistic regression model
+log_model = LogisticRegression(max_iter=1000)
+log_model.fit(X_train, y_train)
+y_pred = log_model.predict(X_test)
+print(classification_report(y_test, y_pred))
+print(confusion_matrix(y_test, y_pred))
+print("Accuracy:", accuracy_score(y_test, y_pred))
