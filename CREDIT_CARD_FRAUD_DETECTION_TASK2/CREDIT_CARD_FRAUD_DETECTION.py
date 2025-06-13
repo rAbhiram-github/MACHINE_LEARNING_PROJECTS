542cb3e911ec8f947d8ca1b8b8321a071075d2db
rAbhiram-github <24f1002043@ds.study.iitm.ac.in>
2025-06-13 13:36:26 -0700

    Update and rename CREDIT_CARD_FRAUD_DETECTION_TASK2.py to CREDIT_CARD_FRAUD_DETECTION.py

--- CREDIT_CARD_FRAUD_DETECTION_TASK2/CREDIT_CARD_FRAUD_DETECTION.py
+++ CREDIT_CARD_FRAUD_DETECTION_TASK2/CREDIT_CARD_FRAUD_DETECTION.py
@@ -2,7 +2,7 @@
 # AUTHOR: ABHIRAM R
 # TASK NAME: CREDIT CARD FRAUD DETECTION
 # TASK CATEGORY: MACHINE LEARNING
-# GITHUB REPOSITORY: https://github.com/rAbhiram-github/CODSOFT/tree/main/CREDIT_CARD_FRAUD_DETECTION_TASK2
+# GITHUB REPOSITORY: https://github.com/rAbhiram-github/Projects/tree/main/CREDIT_CARD_FRAUD_DETECTION_TASK2
 
 # Importing libraries
 import pandas as pd 
